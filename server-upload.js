8477b30d3c88859a3aedf53297b654f48f0e1b0b
Süheyl <suheylseles@gmail.com>
2021-08-22 22:55:27 +0300

    file upload uygulamasında mongoDb ye kayıt tamamlandı.

--- server-upload.js
+++ server-upload.js
@@ -1,11 +1,11 @@
 const express = require('express');
-//const mongoose = require('mongoose');
 const multer = require('multer');
 const path = require('path');
 const uuid = require('uuid').v4;
 const app = express();
+const mongoose = require('mongoose');
+const Image = require('./Models/Image');
 
-/**
 mongoose.connect('mongodb://localhost:27017/fileSave', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -15,24 +15,37 @@ mongoose.connect('mongodb://localhost:27017/fileSave', {
 }).catch(err => {
     console.log("MongoDb'ye bağlanmadı.", err)
 })
-*/
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads')
     },
     filename: (req, file, cb) => {
-        const { originalname } = file;
+        //const { originalname } = file;
         // or 
         // uuid, or fieldname
-        cb(null, originalname);
+        const ext = path.extname(file.originalname);
+        const id = uuid();
+        const filePath = `images/${id}${ext}`;
+        Image.create({ filePath })
+           .then(() => {
+               cb(null, filePath);
+           });
     }
 })
 const upload = multer({ storage }); // or simply { dest: 'uploads/' }
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.static('uploads'));
 
 app.post('/upload', upload.array('avatar'), (req, res) => {
     return res.json({ status: 'OK', uploaded: req.files.length });
+});
+
+app.get('/images', (req, res) => {
+    Image.find()
+      .then((images) => {
+        return res.json({ status : 'OK', images })
+      });
 });
 
 app.listen(3162);
